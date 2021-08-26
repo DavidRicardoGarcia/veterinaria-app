8ec3530e4b6868c5a2ad61cc06472631743148a5
John Doe <sermi.cima@gmail.com>
2021-08-26 00:31:08 -0500

    avance2

--- js/mascotas.js
+++ js/mascotas.js
@@ -1,16 +1,16 @@
 const listaMascotas = document.getElementById('lista-Mascotas');
 //variables del modal
-const tipo=document.getElementById('tipoRaza');
-const nombreDueno=document.getElementById('fnombreD');
-const nombreMascota=document.getElementById('fnombreM');
-const formModal=document.getElementById("form-modal");
-const btnGuardar=document.getElementById("guardar");
-const indiceEditar=document.getElementsByName("indice");
-const ftel=document.getElementsByName("tel");
-const fcolor=document.getElementsByName("color");
-const falergias=document.getElementsByName("color");
-const fgenero=document.getElementsByName("genero");
-const fdireccion=document.getElementsByName("direccion");
+const tipo = document.getElementById('tipoRaza');
+const nombreDueno = document.getElementById('fnombreD');
+const nombreMascota = document.getElementById('fnombreM');
+const formModal = document.getElementById("form-modal");
+const btnGuardar = document.getElementById("guardar");
+const indiceEditar = document.getElementsByName("indice");
+const ftel = document.getElementsByName("tel");
+const fcolor = document.getElementsByName("color");
+const falergias = document.getElementsByName("color");
+const fgenero = document.getElementsByName("genero");
+const fdireccion = document.getElementsByName("direccion");
 
 
 let mascotas = [{
@@ -18,11 +18,11 @@ let mascotas = [{
     nombreD: "David García",
     direccion: "Rio lerma 255, Miguel Hidalgo",
     tel: 3134568746,
-    tipo:"Gato",
-    genero:"Macho",
-    color:"blanco con negro",
-    alergias:"Ninguna",
-    reproductivo:"incompleto",
+    tipo: "Gato",
+    genero: "Macho",
+    color: "blanco con negro",
+    alergias: "Ninguna",
+    reproductivo: "Incompleto",
     nombreM: "Zeus"
 },
 {
@@ -30,11 +30,11 @@ let mascotas = [{
     nombreD: "Felipe Cruz",
     direccion: "Las marias 33, Union",
     tel: 3124589124,
-    tipo:"Gato",
-    genero:"Hembra",
-    color:"gris",
-    alergias:"Ninguna",
-    reproductivo:"incompleto",
+    tipo: "Gato",
+    genero: "Hembra",
+    color: "gris",
+    alergias: "Ninguna",
+    reproductivo: "Incompleto",
     nombreM: "Fiona"
 }];
 
@@ -52,7 +52,7 @@ function listarMascotas() {
     <td>${mascota.alergias}</td>
     <td>${mascota.reproductivo}</td>
     <td>
-    <div class="btn-group" role="group" >   <a name="" id="" class="btn btn-info editar" data-toggle="modal" data-target="#modalCrearMascota" onclick=editar(${index}) href="#" role="button">${mascota.tipo=="Gato" ? `<i class="fa fa-cat" aria-hidden="true"></i>` : `<i class="fa fa-dog" aria-hidden="true"></i>`} Editar</a>
+    <div class="btn-group" role="group" >   <a name="" id="" class="btn btn-info editar" data-toggle="modal" data-target="#modalCrearMascota" onclick=editar(${index}) href="#" role="button">${mascota.tipo == "Gato" ? `<i class="fa fa-cat" aria-hidden="true"></i>` : `<i class="fa fa-dog" aria-hidden="true"></i>`} Editar</a>
         
         <a name="" id="" class="btn btn-danger eliminar" href="#" role="button" onclick=clicEliminar(${index})><i class="fa fa-trash"  aria-hidden="true"></i> Eliminar</a> </div>
    </td>
@@ -67,42 +67,67 @@ function listarMascotas() {
     listaMascotas.innerHTML = htmlMascotas;
     //Array.from(document.getElementsByClassName("eliminar")).forEach((btneliminar,index)=> btneliminar.onclick=clicEliminar(index));
 }
+function limpiadora() {
+    fnombreM.value = "";
+    fnombreD.value = "";
+    tipoRaza.value = "";
+    indice.value = "";
+    genero.value = "";
+    tel.value = "";
+    direccion.value = "";
+    alergias.value = "";
+    btnGuardar.innerHTML = "Crear";
+    color.value = "";
+    reproductivo.value = "";
 
-function enviarModalForm(evento){
+}
+function enviarModalForm(evento) {
     evento.preventDefault();
-    const datos={
-        tipo: tipo.value,
-        nombreD:nombreDueno.value,
-        nombreM:nombreMascota.value
+    
+    const datos = {
+        nombreM:fnombreM.value,
+        tipo:tipoRaza.value,
+        genero: genero.value,
+        tel: tel.value,
+        nombreD:fnombreD.value,
+        direccion:direccion.value,
+        alergias:alergias.value,
+        color:color.value,
+        reproductivo: reproductivo.value,
     }
-    const accion=btnGuardar.innerHTML;
-    console.log(indice.value)
-    if (accion=="Editar") {mascotas[indice.value]=datos;}else {mascotas.push(datos);}
-    fnombreM.value="";
-    fnombreD.value="";
-    tipoRaza.value="";
-    indice.value="";
-    btnGuardar.innerHTML="Crear"
-    
-    
+    limpiadora();
+    const accion = btnGuardar.innerHTML;
+    if (accion == "Editar") { mascotas[indice.value] = datos; } else { mascotas.push(datos); }
+
+
+    console.log(mascotas)
     listarMascotas();
+    
 }
-    function clicEliminar(index){
+function clicEliminar(index) {
     var result = confirm("Seguro que deseas eliminar a la mascota?");
-    if(result){
-     delete mascotas[index];
-     listarMascotas();}
+    if (result) {
+        delete mascotas[index];
+        listarMascotas();
+    }
 }
-function editar(index){
-    btnGuardar.innerHTML="Editar";
+function editar(index) {
+    btnGuardar.innerHTML = "Editar";
     $("modalCrearMascota").modal('toggle');
-    const mascota=mascotas[index];
-    fnombreM.value=mascota.nombreM;
-    fnombreD.value=mascota.nombreD;
-    tipoRaza.value=mascota.tipo;
-    indice.value=index;
+    const mascota = mascotas[index];
+    fnombreM.value=mascota.nombreM,
+    tipoRaza=mascota.tipo.value,
+     genero.value=mascota.genero,
+     tel.value=mascota.tel ,
+    fnombreD.value=mascota.nombreD,
+    direccion.value=mascota.direccion,
+    alergias.value=mascota.alergias,
+    color.value=mascota.color,
+    reproductivo.value=mascota.reproductivo,
+    indice.value = index;
+
 
 }
 listarMascotas();
-formModal.onsubmit=enviarModalForm;
-btnGuardar.onclick=enviarModalForm;
+formModal.onsubmit = enviarModalForm;
+btnGuardar.onclick = enviarModalForm;
