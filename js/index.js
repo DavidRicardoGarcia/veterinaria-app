a3a26db2e1213b6b0741051822219f73ab3abbc6
DAVID <davidnf.44@gmail.com>
2021-09-05 01:53:35 -0500

    union nueva

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const btnBuscar = document.getElementById("btnBuscar");
-
-
+var vector=[];
 
 
 
@@ -102,30 +101,14 @@ if(mascota.did==dueno.id){
   
 });
 
-var consultas={
-  id:1,
-  idc:1,
-  cpeso:5,
-  alergias:"ninguna",
-  observaciones:"Todo bien",
-  diagnostico:"",
-  tipo: "general",
-  estudio:"",
-  resultado:"",
-  fecha:"",
-  a:"",
-  b:"",
-  c:"",
-  d:"",
-  e:"",
-  f:"",
-
-}
+var consultas = [{
+  id:1
+}]
 
 function cuadro(evento) {
-duenos.map(function owner(dueno,index){
-if(dueno.id==evento){
-  let contenidoHTML = `<div class="card w-100" >
+  duenos.map(function owner(dueno, index) {
+    if (dueno.id == evento) {
+      let contenidoHTML = `<div class="card w-100" >
   <div class="card-header">
   <h5>${dueno.nombre}</h5>
   </div>
@@ -165,7 +148,8 @@ if(dueno.id==evento){
     
   </div></td>
   <td><div class="btn-group" role="group" >
-    <button type="button" class="btn btn-info">Ver</button>
+    ${consultas.filter(item=>item.idc==mascota.id).length>0?
+      `<button type="button" class="btn btn-info" onclick="verConsultas(${mascota.id})" data-toggle="modal" data-target="#modal1">Ver</button>`: ""}
     <button type="button" class="btn btn-primary" onclick="nuevaConsulta(${mascota.id})" data-toggle="modal" data-target="#modal1">Nueva</button>
     
   </div></td>
@@ -190,11 +174,11 @@ Nueva mascota
 
 
 
-contenido.innerHTML = contenidoHTML;
-}
-});
+      contenido.innerHTML = contenidoHTML;
+    }
+  });
 
-  
+
 }
 function buscador(index = 0) {
 
@@ -245,8 +229,8 @@ function buscador(index = 0) {
 /* seccion editar dueño */
 function modalEditar(index) {
   let contenidoHTML = document.getElementById("modal1");
-  duenos.map(function ed(persona,idx){
-    if (persona.id==index){
+  duenos.map(function ed(persona, idx) {
+    if (persona.id == index) {
       let contenido = `
       <div class="modal-dialog" role="document">
       <div class="modal-content" >
@@ -271,10 +255,10 @@ function modalEditar(index) {
       </div>
       </div>
       </div>`
-        contenidoHTML.innerHTML = contenido;
+      contenidoHTML.innerHTML = contenido;
     }
   });
-  
+
 }
 function guardarEditar(id) {
 
@@ -300,24 +284,26 @@ function modalNuevaMascota(index) {
 </div>
 <div class="modal-body">
 <div class="form-group">
+
   <label for="petName">Nombre</label>
-  <input type="text" name="" id="petName" class="form-control"  required>
+  <input type="text" name="" id="petName" class="form-control" required >
   <label for="petRaze">Especie</label>
-  <input type="tel" name="" id="petRaze" class="form-control" required >
+  <input type="tel" name="" id="petRaze" class="form-control"  required>
   <label for="petSex">Sexo</label>
   <select id="petSex" class="form-select">
   <option value="hembra">Hembra</option>
   <option value="macho">Macho</option>
 </select>
   <label for="petColor">Color</label>
-  <input type="text" name="" id="petColor" class="form-control" required >
+  <input type="text" name="" id="petColor" class="form-control"  >
   <label for="petAlergy">Alergias</label>
-  <input type="text" name="" id="petAlergy" class="form-control" required >
+  <input type="text" name="" id="petAlergy" class="form-control"  >
+  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+<button type="button" data-dismiss="modal" onclick="guardarMascota()" class="btn btn-primary">Guardar</button>
 </div>
 </div>
 <div class="modal-footer">
-<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-<button type="button" data-dismiss="modal" onclick="guardarMascota()" class="btn btn-primary">Guardar</button>
+
 </div>
 </div>
 </div>
@@ -332,9 +318,9 @@ function guardarMascota() {
   let alergias = document.getElementById("petAlergy").value;
   let sexo = document.getElementById("petSex").value;
   let especie = document.getElementById("petRaze").value;
-  let ids = mascotas.map(item=>parseFloat(item.id));
-  ids=Math.max(...ids);
- 
+  let ids = mascotas.map(item => parseFloat(item.id));
+  ids = Math.max(...ids);
+
   let pet = {
     id: ids + 1,
     did: vID,
@@ -348,7 +334,7 @@ function guardarMascota() {
 
   guardar(pet);
   cuadro(vID);
-  
+
 }
 
 
@@ -360,25 +346,25 @@ function clicEliminar(index) {
   var result = confirm("¿Seguro que deseas eliminar a la mascota?");
   if (result) {
 
-    mascotas.map(function eli(mascota,ind){
-      
-      if(mascota.id==index){
+    mascotas.map(function eli(mascota, ind) {
+
+      if (mascota.id == index) {
         let a = mascotas.splice(0, ind);
         let b = mascotas.splice(1,)
         mascotas = a.concat(b);
-        
+
         eliminar(mascota);
         cuadro(mascota.did);
       }
     })
-    
-   //nueva tabla mascotas
-   
-    
-   
-    
-     
-     
+
+    //nueva tabla mascotas
+
+
+
+
+
+
   }
 
 }
@@ -386,8 +372,8 @@ function clicEliminar(index) {
 /* Editar mascota*/
 function editarMascota(index) {
   let contenidoHTML = document.getElementById("modal1");
-  mascotas.map(function mas(mascota, ind){
-    if(mascota.id==index){
+  mascotas.map(function mas(mascota, ind) {
+    if (mascota.id == index) {
       let contenido = `
   <div class="modal-dialog" role="document">
   <div class="modal-content" >
@@ -425,92 +411,92 @@ function editarMascota(index) {
   </div>
   </div>
   </div>`
-  contenidoHTML.innerHTML = contenido;
+      contenidoHTML.innerHTML = contenido;
     }
   })
- 
-  
+
+
 }
-function guardarEdicion(){
- 
-  let ID=document.getElementById("pID").value;
- let amors=document.getElementById("pDID").value;
-  let nombre= document.getElementById("petName").value;
-  let especie=document.getElementById("petRaze").value;
-  let sexo=document.getElementById("petSex").value;
-  let color=document.getElementById("petColor").value;
-  let alergy=document.getElementById("petAlergy").value;
-   let a=duenos.map((item, index)=> item.id==ID ? index : false);
-  let b=a.filter(item=> item!=false)
-  pet={
-    id:ID,
-    nombre:nombre,
-    especie:especie,
-    sexo:sexo,
-    color:color,
-    alergias:alergy,
-    did:amors
+function guardarEdicion() {
+
+  let ID = document.getElementById("pID").value;
+  let amors = document.getElementById("pDID").value;
+  let nombre = document.getElementById("petName").value;
+  let especie = document.getElementById("petRaze").value;
+  let sexo = document.getElementById("petSex").value;
+  let color = document.getElementById("petColor").value;
+  let alergy = document.getElementById("petAlergy").value;
+  let a = duenos.map((item, index) => item.id == ID ? index : false);
+  let b = a.filter(item => item != false)
+  pet = {
+    id: ID,
+    nombre: nombre,
+    especie: especie,
+    sexo: sexo,
+    color: color,
+    alergias: alergy,
+    did: amors
   };
-  mascotas[b]=pet;
- actualizar(b,pet)
+  mascotas[b] = pet;
+  actualizar(b, pet)
 }
 
-function actualizar(pos,pet){
-  let mD= duenos.map(function f(dueno, index){
-  
-if(pet.did==dueno.id){
-  duenos[index]["mascotas"].map(function g(item, ind){
-    if(pet.id==item.id){duenos[index]["mascotas"][ind]=pet;}
-  });
-  cuadro(dueno.id);
-}
-    
+function actualizar(pos, pet) {
+  let mD = duenos.map(function f(dueno, index) {
 
-  
-});
-}
+    if (pet.did == dueno.id) {
+      duenos[index]["mascotas"].map(function g(item, ind) {
+        if (pet.id == item.id) { duenos[index]["mascotas"][ind] = pet; }
+      });
+      cuadro(dueno.id);
+    }
 
-function guardar(pet){
-  let mD= duenos.map(function f(dueno, index){
-  
-if(pet.did==dueno.id){
-  duenos[index]["mascotas"].push(pet)
-}
-    
 
-  
-});
-}
 
-function eliminar(pet){
-  
-  let mD= duenos.map(function f(dueno, index){
-  
-if(pet.did==dueno.id){
-  duenos[index]["mascotas"].map(function g(item, ind){
-    if(pet.id==item.id){
-      let c=duenos[index]["mascotas"].splice(0,ind);
-      let d=duenos[index]["mascotas"].splice(1,);
-      duenos[index]["mascotas"]=c.concat(d);
-  }
   });
 }
-    
 
-  
-});
+function guardar(pet) {
+  let mD = duenos.map(function f(dueno, index) {
+
+    if (pet.did == dueno.id) {
+      duenos[index]["mascotas"].push(pet)
+    }
+
+
+
+  });
+}
+
+function eliminar(pet) {
+
+  let mD = duenos.map(function f(dueno, index) {
+
+    if (pet.did == dueno.id) {
+      duenos[index]["mascotas"].map(function g(item, ind) {
+        if (pet.id == item.id) {
+          let c = duenos[index]["mascotas"].splice(0, ind);
+          let d = duenos[index]["mascotas"].splice(1,);
+          duenos[index]["mascotas"] = c.concat(d);
+        }
+      });
+    }
+
+
+
+  });
 }
 
 
 
-function nuevaConsulta(index){
-  
+function nuevaConsulta(index) {
+
   let contenidoHTML = document.getElementById("modal1");
-  let fecha=new Date();
+  let fecha = new Date();
 
-  mascotas.map(function cons(mascota, indice){
-    if (mascota.id===index){
-       let contenido = `
+  mascotas.map(function cons(mascota, indice) {
+    if (mascota.id === index) {
+      let contenido = `
       <div class="modal-dialog" role="document">
       <div class="modal-content" >
         <div class="modal-header" >
@@ -533,12 +519,12 @@ function nuevaConsulta(index){
       <label for="observaciones">Observaciones:</label>
       <textarea class="form-control" id="observaciones" rows="3"></textarea>
       <label for="peso" class="col-form-label">Peso:</label>
-      <input type="text" class="form-control" id="peso" >
+      <input type="number" class="form-control" id="peso" >
       </div>
       <label for="selecConsulta" class="col-form-label">Tipo de consulta:</label>
-      <select class="form-select" onchange="cambioTipo(this)" id="selectConsulta">
-      <option selected>Selecciona el tipo de consulta</option>
-      <option value="1">Consulta general</option>
+      <select class="form-select" onchange="cambioTipo(this)" id="selectConsulta" required>
+      <option selected>Open this select menu</option>
+      <option value="1" >Consulta general</option>
       <option value="2">Cirugía</option>
       <option value="3">Estética</option>
       <option value="4">Laboratorio</option>
@@ -549,97 +535,246 @@ function nuevaConsulta(index){
       </div>
       <div class="modal-footer">
       <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-      <button type="button" data-dismiss="modal" onclick="guardarConsulta(${mascota.id})" class="btn btn-primary">Guardar cambios</button>
+      <button type="button" data-dismiss="modal" onclick="guardarConsulta(${mascota.id},${mascota.did})" class="btn btn-primary">Guardar cambios</button>
       </div>
       </div>
       </div>
       </div>`;
-        contenidoHTML.innerHTML = contenido;
+      contenidoHTML.innerHTML = contenido;
 
     }
 
   });
-      
+
 }
 
-function cambioTipo(objeto){
-let kg=document.getElementById("peso").value;
-let agregar=document.getElementById("cambio");
-let valor=parseFloat(objeto.value);
-switch (valor) {
-  case 1:
-    agregar.innerHTML="";
-    let agregarHTML1=`<div class="form-group">
+function cambioTipo(objeto) {
+  let kg = document.getElementById("peso").value;
+  let agregar = document.getElementById("cambio");
+  let valor = parseFloat(objeto.value);
+  switch (valor) {
+    case 1:
+      agregar.innerHTML = "";
+      let agregarHTML1 = `<div class="form-group">
     <label for="diag">Diagnóstico:</label>
     <textarea class="form-control" id="diag" rows="3"></textarea>
   </div>`;
-  agregar.innerHTML=agregarHTML1;   
-    break;
-  case 2:
-    
-    console.log("hola");
-    agregar.innerHTML="";
-    let agregarHTML=` <div class="form-group">
+      agregar.innerHTML = agregarHTML1;
+      break;
+    case 2:
+
+
+      agregar.innerHTML = "";
+      let agregarHTML = ` <div class="form-group">
     <label for="xilazina">Xilazina 10% >10Kg
     </label>
-    <input type="number" class="form-control" id="xilazina" value="${(kg*1.3)/100}">
+    <input type="number" class="form-control" id="xilazina" value="${(kg * 1.3) / 100}">
     <label for="xilazina2">Xilazina % 20 <10Kg</label>
-    <input type="number" class="form-control" id="xilazina2" value="${(kg*1.3)/20}">
+    <input type="number" class="form-control" id="xilazina2" value="${(kg * 1.3) / 20}">
     <label for="Tramadol">Tramadol</label>
-    <input type="number" class="form-control" id="Tramadol" value="${(kg*3)/50}">
+    <input type="number" class="form-control" id="Tramadol" value="${(kg * 3) / 50}">
     <label for="Zelazol">Zelazol
     </label>
-    <input type="number" class="form-control" id="Zelazol" value="${(kg*0.05)}">
+    <input type="number" class="form-control" id="Zelazol" value="${(kg * 0.05)}">
     <label for="Zoletil">Zoletil</label>
-    <input type="number" class="form-control" id="Zoletil" value="${(kg*0.125)}">
+    <input type="number" class="form-control" id="Zoletil" value="${(kg * 0.125)}">
     <label for="Pento">Pento</label>
-    <input type="number" class="form-control" id="Pento" value="${(kg*28/65)}">
+    <input type="number" class="form-control" id="Pento" value="${(kg * 28 / 65)}">
     <label for="Vk">Vitamina K</label>
-    <input type="number" class="form-control" id="Vk" value="${(kg*2/10)}">
+    <input type="number" class="form-control" id="Vk" value="${(kg * 2 / 10)}">
     <small  class="form-text text-muted">Medidas sugeridas con base en el peso y formulas conocidas.</small>
   </div>`;
-  agregar.innerHTML=agregarHTML;    
-    break;
-  case 3:
-    agregar.innerHTML="";
-    break;
-  case 4:
-    agregar.innerHTML="";
-    let agregarHTML2=`<div class="form-group">
+      agregar.innerHTML = agregarHTML;
+      break;
+    case 3:
+      agregar.innerHTML = "";
+      break;
+    case 4:
+      agregar.innerHTML = "";
+      let agregarHTML2 = `<div class="form-group">
     <label for="estudio">Estudio:</label>
     <input type="number" class="form-control" id="estudio">
     <label for="resultado">Resultado:</label>
     <input type="text" class="form-control" id="resultado">
   </div>`;
-  agregar.innerHTML=agregarHTML2;   
-    break;
-  default:
-  //nothing to do here
-    break;
-}
-}
-
-function guardarConsulta(mid){
-
-  let id=mid;
-  let nombre = document.getElementById("petName").value;
-  let color = document.getElementById("petColor").value;
-  let alergias = document.getElementById("petAlergy").value;
-  let sexo = document.getElementById("petSex").value;
-  let especie = document.getElementById("petRaze").value;
-  let ids = mascotas.map(item=>parseFloat(item.id));
-  ids=Math.max(...ids);
- 
-  let pet = {
-    id: ids + 1,
-    did: vID,
-    nombre: nombre,
-    especie: especie,
-    sexo: sexo,
-    color: color,
-    alergias: alergias
+      agregar.innerHTML = agregarHTML2;
+      break;
+    default:
+      //nothing to do here
+      break;
   }
-  mascotas.push(pet);
+}
 
+function guardarConsulta(mid,did) {
+
+  let id = mid;
+  let fecha = document.getElementById("fecha").value;
+  let cespecie = document.getElementById("cespecie").value;
+  let csexo = document.getElementById("csexo").value;
+  let calergia = document.getElementById("calergia").value;
+  let peso = document.getElementById("peso").value;
+  let observaciones = document.getElementById("observaciones").value;
+  let ctipo = document.getElementById("selectConsulta").value;
+  let ids = consultas.map(item => parseFloat(item.id));
+  ids = Math.max(...ids);
+
+  let consul = {
+    id: ids + 1,
+    idc: id,
+    peso: peso,
+    alergias: calergia,
+    observaciones: observaciones,
+    sexo: csexo,
+    especie: cespecie,
+    tipo: ctipo,
+    estudios: "",
+    resultados: "",
+    fecha: fecha,
+    xilazina: "",
+    xilazina2: "",
+    Tramadol: "",
+    Zelazol: "",
+    Zoletil: "",
+    Pento: "",
+    Vk: "",
+    diagnostico: ""
+  }
+
+  switch (parseFloat(ctipo)) {
+    case 1:
+      let diag = document.getElementById("diag").value;
+      consul.diagnostico = diag;
+      consul.tipo="Consulta General";
+      break;
+
+    case 2:
+      let xila1 = document.getElementById("xilazina").value;
+      let xila2 = document.getElementById("xilazina2").value;
+      let tramadol = document.getElementById("Tramadol").value;
+      let Zela = document.getElementById("Zelazol").value;
+      let zole = document.getElementById("Zoletil").value;
+      let pento = document.getElementById("Pento").value;
+      let vk = document.getElementById("Vk").value;
+      consul.xilazina = xila1;
+      consul.xilazina2 = xila2;
+      consul.Tramadol = tramadol;
+      consul.Zelazol = Zela;
+      consul.Zoletil = zole;
+      consul.Pento = pento;
+      consul.Vk = vk;
+      consul.tipo="Cirugía";
+      break;
+    case 3:
+      consul.tipo="Estética";
+      break;
+    case 4:
+      let estudio = document.getElementById("estudio").value;
+      let resultado = document.getElementById("resultado").value;
+      consul.estudios = estudio;
+      consul.resultados = resultado;
+      break;
+    
+  }
+  consultas.push(consul);
+  cuadro(did);
+  
+
+}
+
+
+
+
+function verConsultas(id){
+ 
+  let contenidoHTML = document.getElementById("modal1");
+  let uno,dos;
+  contenidoHTML.innerHTML="";
+  vector=[];
+ 
+  
+  consultas.map(function ed(consulta) {
+    
+    if (consulta.idc == id) {
+     
+      uno = `
+      <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content" >
+        <div class="modal-header" >
+      <h5 class="modal-title" id="">Consultas</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>
+      <div class="modal-body table-responsive">
+          <table class="table">
+  <thead class="thead-dark">
+    <tr>
+    <th scope="col">#</th>
+    <th scope="col">Fecha</th>
+    <th scope="col">Especie</th>
+    <th scope="col">Sexo</th>
+    <th scope="col">Peso</th>
+    <th scope="col">Alergias</th>
+    <th scope="col">Observaciones</th>
+    <th scope="col">Tipo de consulta</th>
+    <th scope="col">Estudio</th>
+    <th scope="col">Resultados</th>
+    <th scope="col">Xilazina 10%>10Kg</th>
+    <th scope="col">Xilazina % 20 <10Kg</th>
+    <th scope="col">Tramadol</th>
+    <th scope="col">Zelazol</th>
+    <th scope="col">Zoletil</th>
+    <th scope="col">Pento</th>
+    <th scope="col">Vitamina K</th>
+    <th scope="col">Diagnóstico</th>
+    </tr>
+  </thead>
+  <tbody>
+  `;
+  
+    }
+  });
+  vector.push(uno);
+  consultas.map(function ed(consulta) {
+    if (consulta.idc == id) {
+     
+      dos = `
+      <tr>
+      <td>${consulta.id}</td>
+      <td>${consulta.fecha}</td>
+      <td>${consulta.especie}</td>
+      <td>${consulta.sexo}</td>
+      <td>${consulta.peso}</td>
+      <td>${consulta.alergias}</td>
+      <td>${consulta.observaciones}</td>
+      <td>${consulta.tipo}</td>
+      <td>${consulta.estudios}</td>
+      <td>${consulta.resultados}</td>
+      <td>${consulta.xilazina}</td>
+      <td>${consulta.xilazina2}</td>
+      <td>${consulta.Tramadol}</td>
+      <td>${consulta.Zelazol}</td>
+      <td>${consulta.Zoletil}</td>
+      <td>${consulta.Pento}</td>
+      <td>${consulta.Vk}</td>
+      <td>${consulta.diagnostico}</td>
+      </tr>
+  `;
+  vector.push(dos);
+    }
+  });
+  vector.push(`</tbody>
+     
+  </div> </table>
+        </div>
+        <div class="modal-footer">
+        <a data-toggle="modal" href="#myModal2" class="btn btn-primary">Launch modal</a>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" data-dismiss="modal"  class="btn btn-primary">Guardar cambios</button>
+        </div>
+        </div>
+        </div>
+        </div>`)
+  contenidoHTML.innerHTML=vector.join("");
 }
 btnBuscar.onclick = buscador;
